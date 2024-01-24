47c3ab5a72f31081b474298f60e57ad0f36a4440
Julio Cesar <julio15.zn@gmail.com>
2024-01-23 23:14:27 -0300

    modulo docker

---
+++ 1-Docker/comandos.sh
@@ -0,0 +1,7 @@
+docker run -d --name nginx-server -p 8080:80 -v C:\Users\julio\Documents\FullCycle\1-Docker\html:/usr/share/nginx/html nginx
+
+docker run -d --name nginx-server -p 8080:80 --mount type=bind,source="$(pwd)",target=/usr/share/nginx/html nginx
+
+docker run -d --name nginx-server -p 8080:80 --mount type=volume,source=meuVolume,target=/app nginx
+
+docker run -d --name nginx-server -p 8080:80 -v meuVolume:/app nginx
